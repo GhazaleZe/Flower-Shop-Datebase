456e89553c82e3e8e5afc4863b880e192d0eb24e
ghazale <ghazalze@yahoo.com>
2020-05-26 11:19:38 +0430

    salesperson is completed

--- Insert.sql
+++ Insert.sql
@@ -3,8 +3,26 @@ Go
 
 select * from SalesPerson
 insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
-values ('961111111','Hasti','Gohari','2948201047','8-12','florist','4400000','1988-12-21','Female','1396-07-12')
+values ('961111111','Hasti','Gohari','2948201047','8-13','florist','5400000','1988-12-21','Female','1396-07-12')
 insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
-values ('9673992237','Pedram','Kamyar','1287589023','8-12','florist','4400000','1993-09-11','male','1396-10-01')
+values ('9673992237','Pedram','Kamyar','1287589023','8-13','florist','5400000','1993-09-11','Male','1396-10-01')
 insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
-values ('9773992237','Mahta','Yari','1286392649','8-12','cashier','4900000','1996-02-11','Female','1397-02-01')
+values ('9773992237','Mahta','Yari','1286392649','8-13','cashier','5000000','1996-02-11','Female','1397-02-01')
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('9373992237','Ahoo','Farmani','1285799221','8-20','ShopPerson','10960000','1992-07-13','Female','1393-01-20')
+
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('941111111','Fateme','Bahrami','1677903328','15-19','florist','5400000','1996-01-21','Female','1394-07-01')
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('9573992237','Armin','Bahrami','1670074271','19-23','florist','5400000','1998-12-11','Male','1395-01-01')
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('9578992217','Amin','Moradi','1279854321','15-23','cashier','7900000','1996-01-19','Male','1395-02-01')
+
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('9378993266','Ramin','Farbod','1286778434','6-10','purchaser','6100000','1996-01-21','Male','1393-07-01')
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('9473955537','Raha','Yousofi','1280074275','6-10','purchaser','6100000','1990-12-11','Female','1394-01-20')
+insert into SalesPerson(ID,first_name,last_name,national_code,working_hour,[role],salary,birthdate,gender,hiring_date)
+values ('9678992212','Amin','Moradi','1279856781','6-10','purchaser','6100000','1993-01-19','Male','1396-02-01')
+
+select * from Colour
