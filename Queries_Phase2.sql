bb83a0a65dbec21514d2b1ae0dbe96fe9bdc7949
ghazale <ghazalze@yahoo.com>
2020-06-04 01:07:30 +0430

    view for figuring out Customer's name

--- Queries_Phase2.sql
+++ Queries_Phase2.sql
@@ -1,8 +1,6 @@
 use FlowerShop
 go
 
-select * from Colour
-select * from Flower
 
 CREATE VIEW [Flower_Color] AS
 select Flower.ID,Flower.Flower_name,Colour.color_name,Colour.Hex_Code
@@ -11,9 +9,8 @@ where Flower.Flower_color_ID=Colour.color_ID
 
 select * from Flower_Color
 
-select * from Buy
-select * from SalesPerson
-select * from GreenHouse
+--************************************************************************************
+
 CREATE VIEW [Responsible_for_the_purchase] AS
 select Buy.ID,Buy.Buy_date,GreenHouse.[name],SalesPerson.first_name,SalesPerson.last_name 
 from Buy, SalesPerson ,GreenHouse
@@ -21,3 +18,11 @@ where Buy.SalesPerson_ID=SalesPerson.ID and Buy.Greenhouse_ID= GreenHouse.ID
 
 select * from Responsible_for_the_purchase
 
+--************************************************************************
+
+CREATE VIEW [Order_with_Customer's_name] AS
+select [Order].ID,[Order].Customer_ID,Customer.first_name,Customer.last_name,[Order].Shop_date
+from [Order],Customer
+where [Order].Customer_ID=Customer.ID
+
+select * from [Order_with_Customer's_name]
