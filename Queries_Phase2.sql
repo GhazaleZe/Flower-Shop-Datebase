db38418ff47add8aa793abd0023e220331b3ef9f
ghazale <ghazalze@yahoo.com>
2020-06-04 00:55:32 +0430

    a view for easy access of Who Buy and when

--- Queries_Phase2.sql
+++ Queries_Phase2.sql
@@ -10,3 +10,14 @@ from Flower,Colour
 where Flower.Flower_color_ID=Colour.color_ID
 
 select * from Flower_Color
+
+select * from Buy
+select * from SalesPerson
+select * from GreenHouse
+CREATE VIEW [Responsible_for_the_purchase] AS
+select Buy.ID,Buy.Buy_date,GreenHouse.[name],SalesPerson.first_name,SalesPerson.last_name 
+from Buy, SalesPerson ,GreenHouse
+where Buy.SalesPerson_ID=SalesPerson.ID and Buy.Greenhouse_ID= GreenHouse.ID
+
+select * from Responsible_for_the_purchase
+
