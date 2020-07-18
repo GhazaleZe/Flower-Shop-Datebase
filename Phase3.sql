4aa67c58347e40f68c3588c899b1cc87c14c1488
ghazale <ghazalze@yahoo.com>
2020-07-18 20:27:26 +0430

    PROCEDURE for calculating prices OK

--- Phase3.sql
+++ Phase3.sql
@@ -4,7 +4,7 @@ select * from FlowersInOrder
 select * from Flower;
 --function to trigger
 
-CREATE PROCEDURE PriceToFlowersInOrder
+CREATE PROCEDURE PriceOK
 	-- Add the parameters for the stored procedure here
 	 @OrderID int
 AS
@@ -18,12 +18,31 @@ BEGIN
 	update FlowersInOrder
 	set Price = Number * t.p
 	from   T,FlowersInOrder
-	where  FlowersInOrder.Order_ID=T.o and FlowersInOrder.Flower_ID=T.f  and FlowersInOrder.Order_ID=@OrderID
+	where  FlowersInOrder.Order_ID=T.o and FlowersInOrder.Flower_ID=T.f  and FlowersInOrder.Order_ID=@OrderID;
+
+	
+	with Ta(o,p) as 
+	(
+		select Order_ID,sum(Price)
+		from FlowersInOrder
+		group by Order_ID
+		having Order_ID=@OrderID
+
+	)
+	UPdate [Order]
+	set Total_Cost=Ta.p+[Order].Wrapping_price
+	from [Order],Ta
+	where [Order].ID=Ta.o and [Order].ID=@OrderID;
+
+
+	UPdate [Order]
+	set Final_Cost= Total_Cost-Discount
+	from [Order]
+	where[Order].ID=@OrderID;
 END
 GO
 
 --exec  PriceToFlowersInOrder(4)
 select * from [Order]
 select * from  FlowersInOrder
-
 
