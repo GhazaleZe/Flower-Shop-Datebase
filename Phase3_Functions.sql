39cf7f527284300faba41bc387459dbdc0b4e6ff
ghazale <ghazalze@yahoo.com>
2020-07-19 01:38:53 +0430

    functions for checking mobile and phone number is added

--- Phase3_Functions.sql
+++ Phase3_Functions.sql
@@ -1,3 +1,66 @@
 USE FlowerShop
 Go
 
+CREATE FUNCTION CheckPhone
+(
+	-- Add the parameters for the function here
+	@HOME varchar(15)
+)
+RETURNS varchar(100)
+AS
+BEGIN
+	DECLARE  @Result varchar(100);
+	Declare @LenH int;
+
+	if (@LenH<>11 or @HOME <> '0%')
+	begin
+		set @Result='Please Enter a Right Phone Number with its city code.';
+	end
+	ElSE
+		set @Result='True';
+	Return @Result
+END
+GO
+CREATE FUNCTION CheckMobile
+(
+	-- Add the parameters for the function here
+	@Mobile varchar(15)
+)
+RETURNS varchar(105)
+AS
+BEGIN
+	DECLARE  @Result varchar(105);
+	Declare @LenM int;
+	set @LenM = LEN(@Mobile);
+	if (@LenM<>11 or @Mobile <> '09%'  )
+	begin
+		set @Result='Please Enter a Right Mobile Number starts with 0';
+	end
+	ElSE
+		set @Result='True';
+	Return @Result
+END
+GO
+select dbo.CheckPhone('33333333333')
+select dbo.CheckMobile('09130759757')
+
+CREATE FUNCTION 
+(
+	-- Add the parameters for the function here
+	@Mobile varchar(15)
+)
+RETURNS varchar(105)
+AS
+BEGIN
+	DECLARE  @Result varchar(105);
+	Declare @LenM int;
+	set @LenM = LEN(@Mobile);
+	if (@LenM<>11 or @Mobile <> '09%'  )
+	begin
+		set @Result='Please Enter a Right Mobile Number starts with 0';
+	end
+	ElSE
+		set @Result='True';
+	Return @Result
+END
+GO
