a6b4dc065607b388be60ac438a8c97dbe3f45fbb
ghazale <ghazalze@yahoo.com>
2020-07-21 01:25:56 +0430

    MyGrid is added

--- interface/main_k.py
+++ interface/main_k.py
@@ -3,9 +3,19 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget()
+
+
 class MyApp(App):
     def build(self):
-        return Label (text="Ghazale is coding")
+        return MyGrid
+
 
 if __name__ == "__main__":
     MyApp().run()
